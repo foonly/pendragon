7a09b421442367fb754d275238049518769b7193
Niklas Schönberg <niko@uplink.fi>
2014-08-23 23:13:18 +0300

    Changed image, boxes back to block. Added credits.

--- include/init.php
+++ include/init.php
@@ -1,6 +1,8 @@
 <?php
 if (!defined("ENTRY")) exit("Invalid Entry Point");
 
+header ("Content-Type: text/html; charset=UTF-8");
+
 define("APATH",getcwd());
 
 require "randomtable/randomtable.class.php";
