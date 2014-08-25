246511b577fd53c127da1eb93ec67831b8fbbe1d
Niklas Schönberg <niko@uplink.fi>
2014-08-25 10:46:15 +0300

    Added some default values.

--- include/settings.default.php
+++ include/settings.default.php
@@ -2,9 +2,9 @@
 if (!defined("ENTRY")) exit("Invalid Entry Point");
 
 define("APATH",getcwd());
-define("RPATH",'');
-define("TITLE",'');
-define("HEADER",'');
-define("FOOTER",'');
+define("RPATH",'/');
+define("TITLE",'Pendragon');
+define("HEADER",'Pendragon Random Generators');
+define("FOOTER",'Pendragon Random Generators - by Dr. J.M. "Thijs" Krijger & Niklas Schönberg - Hosted by Uplink Data Finland Oy Ab ');
 
 define("SECRET",'');
